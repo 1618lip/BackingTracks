77035735adb0072405b6b77cd31e0cfc1872f5ff
Philip Pincencia <philip.m.pincencia@gmail.com>
2023-12-21 19:45:33 -0800

    Edit index.html

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 1618;
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Handle other routes or middleware as needed
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
